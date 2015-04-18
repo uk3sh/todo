53b18080b5a56dd11858b4899dbc76eb0fec9763
Ukesh Kumar <godsfear@gmail.com>
2015-04-18 15:20:58 +0530

    Added validations to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,10 @@
 class Task < ActiveRecord::Base
-belongs_to :user
+	belongs_to :user
 
-auto_html_for :content do
+	validates :user_id, presence: true
+	validates :content, presence: true
+
+	auto_html_for :content do
 	html_escape
 	image
 	youtube(:width => "100%", :height => 250, :autoplay => false)
