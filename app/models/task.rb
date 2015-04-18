2c8094cdb8dc0bd230b72e66a6c490b47cf334c7
Ukesh Kumar <godsfear@gmail.com>
2015-04-18 17:19:56 +0530

    Fixed task partial

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,10 +5,10 @@ class Task < ActiveRecord::Base
 	validates :content, presence: true
 
 	auto_html_for :content do
-	html_escape
-	image
-	youtube(:width => "100%", :height => 250, :autoplay => false)
-	link :target => "_blank", :rel => "nofollow"
-	simple_format
+		html_escape
+		image
+		youtube(:width => "100%", :height => 250, :autoplay => false)
+		link :target => "_blank", :rel => "nofollow"
+		simple_format
 	end
 end
