20fcc0ea79228a7880f9885b853dc984dd150bb4
Ukesh Kumar <godsfear@gmail.com>
2015-04-18 10:59:55 +0530

    Added associations and edited tasks controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417154120) do
+ActiveRecord::Schema.define(version: 20150418050746) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
